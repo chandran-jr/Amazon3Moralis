b9a8b3a00815213c333850a7eaa448fbef59fc8c
Govind B Chandran <govindchandran150@gmail.com>
2022-04-12 01:07:46 +0530

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button, Input, Space, Badge } from 'antd';
 import { useMoralis } from "react-moralis";
 import './Header.css'
+import Amazon from '../images/logo.png';
+import BookStore from '../images/bookstore.png';
+import USA from '../images/usa.png';
+import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+
+const {Search} = Input;
 
 const Header = () => {
   const { authenticate } = useMoralis();
@@ -8,11 +14,32 @@ const Header = () => {
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
         ghost={false}
-        title="Amazon Clone"
         extra={[
-         <Button key="1" type="primary" onClick={() => authenticate()}>
-          Login with Metamask
+          <>
+          <img src={Amazon} className="logo" alt="logo" />
+          <img src={BookStore} className="logo" alt="logo" />
+          <Search
+          placeholder="Find a product"
+          enterButton
+          className="searchBar"/>
+         <Button className="login" key="1" type="primary" onClick={() => authenticate()}>
+          Login
           </Button>
+
+          <Space size={"large"}>
+              
+              <Badge count={0} showZero>
+                <span className="header-buttons">
+                  <ShoppingCartOutlined className="header-icon" />
+                  Cart
+                </span>
+              </Badge>
+              <Space className="header-buttons" size={"small"}>
+                <img src={USA} alt="region" className="flag"></img>▾
+              </Space>
+              
+            </Space>
+          </>
         ]}>
       </PageHeader>
     </div>
