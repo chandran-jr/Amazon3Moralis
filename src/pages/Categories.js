77310e511d4fa017c77fdf6ae84840b4658948ef
Govind B Chandran <govindchandran150@gmail.com>
2022-04-22 21:59:36 +0530

    Add files via upload

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useLocation } from 'react-router-dom';
 
 const Categories = () => {
+
+  const {state: category} = useLocation()
 
 return(
   <>
   <div className="container">
-    <Button type="primary">Categories Page</Button>
+    <Button onClick = {() => console.log(category)} type="primary">Categories Page</Button>
   </div>
   </>
 )
